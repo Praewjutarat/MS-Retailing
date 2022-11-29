9f96d9c218b0a6a675762b48b401c7f9f144f942
Praewjutarat <jutarat1215@gmail.com>
2022-11-29 23:49:27 +0700

    commit

--- Admin_customer_datagrid/Admin_customer_dpcon.php
+++
@@ -1,7 +0,0 @@
-//dbcon.php
-<?php
-$conn = new mysqli('localhost','root','','ma_retailing');
-if ($conn->connect_error) {
-    die('Error : ('. $conn->connect_error .') '. $conn->connect_error);
-}
-?>
